17a019c2630194c422571c24032ff7e4beba1486
Michael Skrynski <zlorfi@gmail.com>
2019-03-12 10:04:42 +0100

    initial commit

---
+++ keyboards/xd75/keymaps/michi/rules.mk
@@ -0,0 +1,7 @@
+# QMK Build Options
+#   change to "no" to disable the options, or define them in the Makefile in 
+#   the appropriate keymap folder that will get included automatically
+#
+UNICODE_ENABLE = yes        # Unicode
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight. 
+TAP_DANCE_ENABLE = yes      # Enable tap dance
