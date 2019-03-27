a771fde564be720608daf63f6b93e74a5519b1e8
Michael Skrynski <zlorfi@gmail.com>
2019-03-27 12:49:54 +0100

    reset button added

--- keyboards/xd75/keymaps/michi/rules.mk
+++ keyboards/xd75/keymaps/michi/rules.mk
@@ -7,3 +7,5 @@ RGBLIGHT_ENABLE  = yes # Enable WS2812 RGB underlight.
 TAP_DANCE_ENABLE = no  # Enable tap dance
 LAYER_SWITCH_RGB = no  # Enable layer based RGBLIGHT (this is very expensive action, use with caution)
 COMMAND_ENABLE   = no  # Disable command in order to use Space Cadet Shift Enter
+MOUSEKEY_ENABLE  = no  # Disable mouse emulation
+TERMINAL_ENABLE  = no  # Disable terminal
